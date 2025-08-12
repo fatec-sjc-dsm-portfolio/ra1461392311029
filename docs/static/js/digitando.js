5c17e48fd9286aadc2223d572ec5c8d1c08628c1
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2025-08-12 18:25:43 -0300

    Ajustes

---
+++ docs/static/js/digitando.js
@@ -0,0 +1,22 @@
+function typeEffect(element, text, speed, callback) {
+    let i = 0;
+    function typing() {
+        if (i < text.length) {
+            element.textContent += text.charAt(i);
+            i++;
+            setTimeout(typing, speed);
+        } else if (callback) {
+            callback();
+        }
+    }
+    typing();
+}
+
+// Pega os elementos
+const nameElement = document.getElementById("typing-name");
+const roleElement = document.getElementById("typing-role");
+
+// Aplica o efeito
+typeEffect(nameElement, "Silmara Bittencourt", 100, function() {
+    typeEffect(roleElement, "Desenvolvedora Full-Stack | PHP | Laravel", 80);
+});
