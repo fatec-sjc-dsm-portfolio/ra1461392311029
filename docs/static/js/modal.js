367d2747e4d27ee6348112c61b8850610c24cf70
Silmara Bittencourt <silmarabittencourt13@gmail.com>
2025-09-23 10:43:24 -0300

    Adicionando 2° e 3° semestre

--- docs/static/js/modal.js
+++ docs/static/js/modal.js
@@ -1,3 +1,4 @@
+// Modal pro projeto 1
 const modal = document.getElementById('modal');
 const projeto1 = document.getElementById('projeto1');
 const closeModal = document.getElementById('close-modal');
@@ -15,3 +16,42 @@ window.addEventListener('click', (event) => {
         modal.style.display = 'none';
     }
 });
+
+// Modal pro projeto 2
+const modal2 = document.getElementById('modal2');
+const projeto2 = document.getElementById('projeto2');
+const closeModal2 = document.getElementById('close-modal2');
+
+projeto2.addEventListener('click', () => {
+    modal2.style.display = 'block';
+});
+
+closeModal2.addEventListener('click', () => {
+    modal2.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if(event.target == modal2) {
+        modal2.style.display = 'none';
+    }
+});
+
+// Modal pro projeto 3
+const modal3 = document.getElementById('modal3');
+const projeto3 = document.getElementById('projeto3');
+const closeModal3 = document.getElementById('close-modal3');
+
+projeto3.addEventListener('click', () => {
+    modal3.style.display = 'block';
+});
+
+closeModal3.addEventListener('click', () => {
+    modal3.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if(event.target == modal3) {
+        modal3.style.display = 'none';
+    }
+});
+
